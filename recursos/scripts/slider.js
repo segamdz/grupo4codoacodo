42fa0bdc9d7778ba394133387454dc6df3b14937
Enrique Alberto Sifontes Romo <easr97@hotmail.com>
2024-05-12 00:28:35 -0400

    Arreglado manejo de fonts en la forma de contacto para hacerla responsiva

--- recursos/scripts/slider.js
+++ recursos/scripts/slider.js
@@ -192,8 +192,37 @@ let slider = function (sliderElement) {
     if (document.activeElement.className == "cont") {
       gotoSlide("#contacto");
     }
-    let collection = document.getElementsByClassName("slider__indicator");
-    console.log('collection:', collection);
-
+    if (document.activeElement.className == "alpi") {
+      gotoSlide("#pie");
+    }
   })
+
+  function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /*loop through a collection of all HTML elements:*/
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+      elmnt = z[i];
+      /*search for elements with a certain atrribute:*/
+      file = elmnt.getAttribute("w3-include-html");
+      if (file) {
+        /*make an HTTP request using the attribute value as the file name:*/
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+          if (this.readyState == 4) {
+            if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+            if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+            /*remove the attribute, and call this function once more:*/
+            elmnt.removeAttribute("w3-include-html");
+            includeHTML();
+          }
+        }
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        /*exit the function:*/
+        return;
+      }
+    }
+  };
+
 }
